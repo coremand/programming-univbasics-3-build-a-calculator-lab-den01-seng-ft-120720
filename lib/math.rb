2b320ac157ee3e037b899a9cf028762428c5daca
Obinna Nwabia <valinevite@gmail.com>
2020-11-09 03:18:31 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,15 +1,15 @@
 def addition(num1, num2)
-  sum = num1 + num2
-  puts sum
-  return sum 
+  sum = num2 + num1
+  p sum
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  p difference
 end
 
 def division(num1, num2)
-
+ div = num2 
 end
 
 def multiplication(num1, num2)
