d092264f02102c0ef156d7e86be79cda22d249f5
Obinna Nwabia <valinevite@gmail.com>
2020-11-09 03:23:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,17 +9,21 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
- div = num2 
+ div = num2 / num1
+ p div
 end
 
 def multiplication(num1, num2)
-
+  mult = num1 * num2
+  p mult
 end
 
 def modulo(num1, num2)
-
+  remainder = num2 % num1
+  p remainder
 end
 
 def square_root(num)
-
+  root = Math.sqrt(num)
+  p root
 end
